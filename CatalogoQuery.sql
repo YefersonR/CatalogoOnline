426bad0ee4450b9dd0ffef0ecb1cbfb71cdf4fe6
YefersonR <Yefersonrubio27@gmail.com>
2022-08-25 23:44:18 -0400

    Add SqlQuery

---
+++ CatalogoQuery.sql
@@ -0,0 +1,190 @@
+Create Database CatalogoOnline
+use CatalogoOnline
+
+Create table Users
+(
+ID int primary key identity(1,1),
+FirstName varchar(20),
+LastName varchar(30),
+PhoneNumber varchar(15),
+Email varchar(50),
+Addres varchar(60),
+UserName varchar(40),
+UserPassword varchar(30),
+IsActive bit
+)
+go
+
+Create table Products(
+ID int primary key identity(1,1),
+ProductName varchar(40),
+UnitPrice Money,
+UnitInStock int,
+Garantie text,
+Discontinued bit,
+CategoryID int
+)
+go
+
+Create table Category(
+ID int primary key identity(1,1),
+CategoryName varchar(20),
+CategoryDescription text,
+IsActive bit
+)
+go
+
+--Inserts
+CREATE PROCEDURE InsertCategory
+	@CategoryName varchar(20),
+	@CategoryDescription text
+AS
+	insert into Category
+	values(@CategoryName,@CategoryDescription,1);
+GO
+
+CREATE PROCEDURE InsertProduct
+	@ProductName varchar(40),
+	@UnitPrice Money,
+	@UnitInStock smallint,
+	@Garantie text,
+	@Discontinued bit,
+	@CategoryID int
+AS
+	insert into Products
+	values(@ProductName,@UnitPrice,@UnitInStock,@Garantie,@Discontinued,@CategoryID);
+GO
+
+CREATE PROCEDURE InsertUser
+	@FirstName varchar(20),
+	@LastName varchar(30),
+	@PhoneNumber varchar(15),
+	@Email varchar(50),
+	@Address varchar(60),
+	@UserName varchar(40),
+	@UserPassword varchar(30),
+	@IsActive bit
+AS
+	insert into Users
+	values(@FirstName,@LastName,@PhoneNumber,@Email,@Address,@UserName,@UserPassword,@IsActive);
+GO
+
+
+--Deletes
+
+CREATE PROCEDURE DeleteProducts
+	@ID int 
+AS
+	DELETE FROM Products WHERE ID = @ID;
+GO
+
+CREATE PROCEDURE DeleteCategory
+	@ID int 
+AS
+	DELETE FROM Category WHERE ID = @ID;
+GO
+
+CREATE PROCEDURE DeleteUser
+	@ID int 
+AS
+	DELETE FROM Users WHERE ID = @ID;
+GO
+--Updates
+CREATE PROCEDURE SetProducts
+	@ID int,
+	@ProductName varchar(40),
+	@UnitPrice Money,
+	@UnitInStock smallint,
+	@Garantie text,
+	@Discontinued bit,
+	@CategoryID int
+
+AS
+	UPDATE Products
+	SET ProductName = @ProductName,
+	UnitPrice = @UnitPrice,
+	UnitInStock = @UnitInStock,
+	Garantie = @Garantie,
+	Discontinued= @Discontinued,
+	CategoryID = @CategoryID
+
+	WHERE ID = @ID;
+GO
+
+CREATE PROCEDURE SetCategorys
+	@ID int,
+	@CategoryName varchar(20),
+	@CategoryDescription text,
+	@IsActive bit
+AS
+	UPDATE Category
+	SET CategoryName = @CategoryName,
+	CategoryDescription = @CategoryDescription,
+	IsActive= @IsActive
+	WHERE ID = @ID;
+GO
+
+CREATE PROCEDURE SetUsers
+	@ID int ,
+	@FirstName varchar(20),
+	@LastName varchar(30),
+	@PhoneNumber varchar(15),
+	@Email varchar(50),
+	@Address varchar(60),
+	@UserName varchar(40),
+	@UserPassword varchar(30),
+	@IsActive bit
+AS
+	UPDATE Products
+	SET @FirstName = @FirstName,
+	@LastName = @LastName,
+	@PhoneNumber = @PhoneNumber,
+	@Email = @Email,
+	@Address= @Address,
+	@UserName= @UserName,
+	@UserPassword= @UserPassword,
+	@IsActive= @IsActive
+	WHERE ID = @ID;
+GO
+
+--Get
+CREATE PROCEDURE GetAllProducts
+AS
+	select * from Products
+GO
+
+CREATE PROCEDURE GetAllCategory
+AS
+	select * from Category
+GO
+
+CREATE PROCEDURE GetCategoryById
+	@ID int
+AS
+	select * from Category where ID = @ID
+GO
+
+CREATE PROCEDURE GetProductById
+	@ID int
+AS
+	select * from Products where ID = @ID
+GO
+
+CREATE PROCEDURE SearchProducts
+	@ProductName varchar(40)
+AS
+	select * from Products where ProductName like '%'+@ProductName+'%' 
+GO
+
+CREATE PROCEDURE FilterByCategory
+	@CategoryID int
+AS
+	select * from Products where CategoryId = @CategoryId  
+GO
+
+CREATE PROCEDURE UserLogin
+	@UserName varchar(40),
+	@UserPassword varchar(30)
+AS
+	select * from Users where UserName = @UserName and UserPassword = @UserPassword
+GO
---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Proyecto
+Proyecto web que se encarga de administrar productos y categorias
+
+El proyecto consta de 3 capas :
+
+*Capa DataLayer se encarga de la interaccion con la base de datos, contiene los modelos de datos de cada tabla y contiene los repositorios donde se realizan las operaciones a la base de datos
+
+*Capa BusinessLayer se encarga de tomar los datos proporcionados por capa DataLayer y procesarlos para la capa de presentacion y en caso de agregar mas logica a la aplicacion este seria el lugar indicado para hacerlo. Esta capa contiene los servicios para administrar los metodos que se podran utilizar en la presentacion y los ViewModel para manejar las peticiones del usuario.
+
+*Capa Presentacion(CatalogoOnline) se encarga de la interaccion con el usuario. Consta de 1 controlador para cada entidad con los metodos que seran proporcionados al usuario y las vistas para determinar la forma en que se mostraran los datos al usuario.
+
+Los servicios contienes todas las funcionalidades CRUD para cada entidad o modulos pero en la pagina web solo contiene interaccion con los productos y un apartado para crear categorias. La pagina web contiene un listado de todos los productos, un buscador de productos, un filtrado por categoria y un apartado para crear productos y categorias.
+
+Las consultas a la base de datos fueron realizadas en ADODB con stores procedures.
+
+Las peticiones fueron hechas con ajax usando jquery y las validaciones de los formularios fueron hechas con jquery validate.
+
+## Uso
+
+1.Ejecutar el script de la base de dato.
+2.Iniciar el programa
+3.Crear categorias para poder asignarselas a los productos
+3.Crear productos con los campos requeridos
+*Para usar el buscador escriba el nombre del producto que desea buscar y presione enter
+*Para filtar por categoria debe seleccionar la categoria y presionar el boton filter 
+
